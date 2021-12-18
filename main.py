7afaf406c6c6f6a4d11a98ce2b784e019459679b
daniele21 <danielemoltisanti@live.it>
2021-12-18 23:33:19 +0100

    optimization tab

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource, DateRangeSlider
+import argparse
+
+from bokeh.io import curdoc, output_file, save
 
 from scripts.paths import TICKER_DETAILS_PATH, TICKER_DATA_DIR, TRANSACTION_PATH
 from scripts.portfolio.portfolio import Portfolio
@@ -7,18 +8,37 @@ from scripts.portfolio.tickers import Tickers
 from scripts.visualization.dashboard import FinanceDashboard
 from scripts.visualization.panel import tab_figures
 
-tickers = Tickers(TICKER_DETAILS_PATH, TICKER_DATA_DIR)
-portfolio = Portfolio(TRANSACTION_PATH)
-dashboard = FinanceDashboard(tickers, portfolio)
 
-tickers.update_tickers_data()
+def main(arguments):
+    title = 'Portfolio Analysis'
 
-fig = tab_figures({
-    'Stake': dashboard.stake_status_plot(),
-    'Performance': dashboard.ticker_performance_plot(),
-    "History": dashboard.ticker_data_plot()
-})
+    tickers = Tickers(TICKER_DETAILS_PATH, TICKER_DATA_DIR)
+    portfolio = Portfolio(TRANSACTION_PATH)
+    dashboard = FinanceDashboard(tickers, portfolio)
+    # tickers.update_tickers_data()
 
-# curdoc().theme = 'dark_minimal'
-curdoc().add_root(fig)
-curdoc().title = "Portfolio Analysis"
+    fig = tab_figures({
+        'Stake': dashboard.stake_status_plot(),
+        'Performance': dashboard.ticker_performance_plot(),
+        "History": dashboard.ticker_data_plot(),
+        "Optimization": dashboard.portfolio_optimization()
+    })
+
+    if arguments.output is not None:
+        output_file(filename=arguments.output, title=title)
+        save(fig)
+        print(f' > Output saved at {arguments.output}')
+    else:
+        # curdoc().theme = 'dark_minimal'
+        curdoc().add_root(fig)
+        curdoc().title = title
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-o', '--output', help='Output file')
+
+    args = parser.parse_args()
+
+    main(args)
