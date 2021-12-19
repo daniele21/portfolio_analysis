88e92c2dda9911959eef46dc20b1e38426b5ffbc
daniele21 <danielemoltisanti@live.it>
2021-12-19 20:08:16 +0100

    main just with html generation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 
-from bokeh.io import curdoc, output_file, save
+from bokeh.io import output_file, save
 
 from core.portfolio.portfolio import Portfolio
 from core.portfolio.tickers import Tickers
@@ -18,7 +18,9 @@ def main(arguments):
     tickers = Tickers(ticker_details_path, TICKER_DATA_DIR)
     portfolio = Portfolio(transactions_path)
     dashboard = FinanceDashboard(tickers, portfolio)
-    tickers.update_tickers_data()
+
+    if arguments.update:
+        tickers.update_tickers_data()
 
     fig = tab_figures({
         'Stake': dashboard.stake_status_plot(),
@@ -28,24 +30,24 @@ def main(arguments):
     })
 
     # Generate output html
-    if arguments.output is not None:
-        output_file(filename=arguments.output, title=title)
-        save(fig)
-        print(f' > Output saved at {arguments.output}')
+    output_file(filename=arguments.output, title=title)
+    save(fig)
+    print(f' > Output saved at {arguments.output}')
 
     # Running Server
-    else:
-        # curdoc().theme = 'dark_minimal'
-        curdoc().add_root(fig)
-        curdoc().title = title
+    # else:
+    #     # curdoc().theme = 'dark_minimal'
+    #     curdoc().add_root(fig)
+    #     curdoc().title = title
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-d', '--tickers', help='Tickers detail filepath')
-    parser.add_argument('-t', '--transactions', help='Transactions csv filepath')
-    parser.add_argument('-o', '--output', help='Output file')
+    parser.add_argument('-d', '--tickers', required=True, help='Tickers detail filepath')
+    parser.add_argument('-t', '--transactions', required=True,  help='Transactions csv filepath')
+    parser.add_argument('-o', '--output', required=True, default='outputs/portfolio_analysis.html',  help='Output filepath')
+    parser.add_argument('-u', '--update', required=True, action='store_true', help='Update historical data')
 
     args = parser.parse_args()
 
