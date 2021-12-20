383ea61d80474fa19e5600404c327deb28b6bd75
francescodisalvo05 <francesco.disalvo99@gmail.com>
2021-12-20 14:21:48 +0100

    fix start_datetime

--- scripts/data/yahoo_extraction.py
+++ scripts/data/yahoo_extraction.py
@@ -18,9 +18,11 @@ def extract_data(ticker: Text,
         end_date = str(yesterday())
 
     end_datetime = datetime.datetime.strptime(end_date, date_format)
-    start_datetime = datetime.datetime.strptime(start_date, date_format)
 
-    end_date = end_date if end_datetime > start_datetime else None
+    if start_date:
+        start_datetime = datetime.datetime.strptime(start_date, date_format)
+
+    end_date = end_date if start_date and end_datetime > start_datetime else None
 
     data = pdr.get_data_yahoo(ticker,
                               start=start_date,
