01e57e136b47ecf8b06ed499135951a62b63977a
daniele21 <danielemoltisanti@live.it>
2025-01-08 11:01:00 +0100

    cleaning

--- portfolio_analysis/scripts/constants/constants.py
+++
@@ -1,6 +0,0 @@
-# OPTIMIZATIONS
-RISK_FREE_RATE = 0.03
-
-# VISUALIZATION
-#TOOLS = 'pan,wheel_zoom,hover,reset,save,crosshair'
-TOOLS = "pan,box_zoom,reset,save,hover"
--- portfolio_analysis/scripts/constants/tickers.py
+++
@@ -1,9 +0,0 @@
-ETF_TICKERS = ['AIAI.MI', 'BATT.MI', 'ECAR.MI',
-               'SWDA.MI', 'USPY.DE', 'WHEA.MI',
-               'YODA.MI', '2B77.DE', 'ESPO.MI',
-               'IWVL.MI', 'QDVE.DE']
-
-STOCK_TICKERS = ['TIT.MI', 'TL0.DE']
-
-CRYPTO_TICKERS = ['ADA-EUR', 'BTC-EUR', 'CRO-EUR',
-                  'ETH-EUR', 'FET-EUR']
